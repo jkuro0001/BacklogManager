aea170e0dfc4271fbb6dedd8bc2a201b7c49721a
Mason <munizm@rpi.edu>
2025-02-16 19:36:30 -0500

    Creating react app
    Using Next.js, react app is under BacklogManager/backlog-manager and contains source code for Create Account page

---
+++ backlog-manager/src/app/components/TopButtons.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/top-buttons.css";
+
+const TopButtons = () => {
+  return (
+    <div className="top-buttons">
+      <button>Home Page</button>
+    </div>
+  );
+};
+
+export default TopButtons;
