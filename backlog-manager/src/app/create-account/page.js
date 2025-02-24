fa330524b533e2005693967a4c850ddb85ab301d
jkuro0001 <alternative00205@gmail.com>
2025-02-23 19:15:22 -0500

    Add files via upload

--- backlog-manager/src/app/create-account/page.js
+++ backlog-manager/src/app/create-account/page.js
@@ -1,11 +1,13 @@
 import TopButtons from "../components/TopButtons";
 import CreateAccount from "../components/CreateAccount";
+import Login from "../components/Login";
 
 export default function CreateAccountPage() {
   return (
     <main>
       <TopButtons />
       <CreateAccount />
+      <Login />
     </main>
   );
 }
